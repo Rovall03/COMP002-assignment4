755d2ad5180034976a55f88992e6679e70a0b65f
rosa ovalle <you@example.com>
2024-11-07 16:32:59 -0800

    in place array added

--- script.js
+++ script.js
@@ -43,5 +43,18 @@ for(let i = array.length - 1; i >= 0; i--){
 }
 return results;
 }
+
+function reverseArrayInPlace(array){
+    for (let i = 0; i < Math.floor(array.length / 2); i++) {
+        let old = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = old;
+      }
+      return array;
+    } 
  
 console.log(reverseArray(["A", "B", "C"]));
+
+let arrayValue =[1,2,3,4,5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
